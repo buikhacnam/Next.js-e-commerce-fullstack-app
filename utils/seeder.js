181defd6a10c96a348d8339f91944fbc8b56d9a7
Bui Nam <buikhacnam11@gmail.com>
2021-11-20 06:10:36 +0700

    some changes

--- utils/seeder.js
+++ utils/seeder.js
@@ -2,7 +2,7 @@ const Room = require('../models/room')
 const mongoose = require('mongoose')
 const rooms = require('../data/rooms')
 
-mongoose.connect('mongodb+srv://dopestuff:mrbui123456@cluster0.qcbff.mongodb.net/next-cruise?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DB_LOCAL_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then((con) => console.log('Connected to Local MongoDB')).catch((err) => console.log(err))
